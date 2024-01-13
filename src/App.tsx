5fc340714770df3e0a4b8a9f99c26d3edabaf5e7
Anton Lugom <mogul086@mail.ru>
2024-01-13 16:51:05 +0300

    base page layout - grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div className="App-wrapper">
+      <Header />
+      <NavBar />
+      <Profile />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img
+        src="https://i.pinimg.com/736x/6e/8e/74/6e8e740911f5fc1887f152425624ef37.jpg"
+        alt="logo"
+      />
+    </header>
+  );
+}
+
+export default Header;
